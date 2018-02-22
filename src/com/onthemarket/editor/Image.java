62f6100a4e831734ef2b184ce30d40f8879deb94
Milan Bojovic <milanbojovic@gmail.com>
2018-02-23 00:34:54 +0100

    Added coordinate colour validations

--- src/com/onthemarket/editor/Image.java
+++ src/com/onthemarket/editor/Image.java
@@ -4,12 +4,19 @@ public class Image {
     private final int M, N;
     private char[][] data;
 
+    public char getData(int x, int y) {
+        validateRange(x, y);
+        x--;
+        y--;
+        return data[x][y];
+    }
+
     public Image(int m, int n) {
         M = m;
         N = n;
+        validateRange(m, n);
 
         data = new char[M][N];
-
         clearImage();
     }
 
@@ -22,6 +29,9 @@ public class Image {
     }
 
     public void drawPixel(int x, int y, char colour) {
+        validateRange(x, y);
+        validateColour(colour);
+
         x--;
         y--;
 
@@ -29,6 +39,9 @@ public class Image {
     }
 
     public void drawVerticalLine(int x, int y1, int y2, char colour) {
+        validateRange(x, y1, y2);
+        validateColour(colour);
+
         x--;
         y1--;
         y2--;
@@ -39,6 +52,9 @@ public class Image {
     }
 
     public void drawHorizontalLine(int x1, int x2, int y, char colour) {
+        validateRange(x1, x2, y);
+        validateColour(colour);
+
         y--;
         x1--;
         x2--;
@@ -50,11 +66,13 @@ public class Image {
 
     public void fillRegion(int x, int y, char newColour, char baseColour) {
         if (baseColour == 'x') {
+            validateRange(x, y);
+            validateColour(newColour);
             x--;
             y--;
             baseColour = data[x][y];
         } else {
-            if (x < 0 || y < 0 || x > M - 1 || y > N - 1 ) return;
+            if (x < 0 || y < 0 || x > M - 1 || y > N - 1) return;
             if (data[x][y] != baseColour) return;
         }
         data[x][y] = newColour;
@@ -79,7 +97,18 @@ public class Image {
             }
             sb.append("\n");
         }
-
         return sb.toString();
+    }
+
+    private void validateRange(int... args) {
+        for (int arg : args) {
+            if (arg < 1 || arg > 250)
+                throw new IllegalArgumentException("Error - Table range validation failed: allowed range: [1, 250].");
+        }
+    }
+
+    private void validateColour(char colour) {
+        if (!Character.isLetter(colour) || !Character.isUpperCase(colour))
+            throw new IllegalArgumentException("Error - Colour validation failed: Colour must be capital letter !!!");
     }
 }
