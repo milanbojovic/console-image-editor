3d5896c1515ffa844cd3afffb8a65b215f319640
Milan Bojovic <milanbojovic@gmail.com>
2018-02-23 00:34:17 +0100

    Added tests and cleared main

--- src/com/onthemarket/Main.java
+++ src/com/onthemarket/Main.java
@@ -7,24 +7,5 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Image editor started, please choose your option: ");
-
-        Image image = new Image(5,6);
-
-        image.drawPixel(2, 3, 'A');
-
-        System.out.println(image);
-
-        image.drawPixel(1, 5, 'P');
-        image.drawHorizontalLine(2, 5, 5, 'B');
-
-        image.fillRegion(3,3,'J', 'x');
-        image.drawVerticalLine(2, 3, 4, 'W');
-        image.drawHorizontalLine(3,4,2,'Z');
-
-
-        System.out.println(image);
-
     }
 }
--- src/com/onthemarket/test/ImageTest.java
+++ src/com/onthemarket/test/ImageTest.java
@@ -1,19 +1,65 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+package com.onthemarket.test;
+
+import com.onthemarket.editor.Image;
+import sun.jvm.hotspot.utilities.Assert;
 
 import static org.junit.Assert.*;
 
-@RunWith(Arquillian.class)
 public class ImageTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(com.onthemarket.editor.Image.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+
+    Image image = null;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        image = new Image(5,6);
     }
 
+    @org.junit.Test
+    public void NewImage() {
+        Assert.that(image.toString().equals("OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\n"), "Error - image is not in expected format.");
+    }
+
+    @org.junit.Test
+    public void drawPixel() {
+        image.drawPixel(3, 3, 'A');
+
+        Assert.that(image.getData(3,3) == 'A', "Error - image validation failed");
+    }
+
+    @org.junit.Test
+    public void drawVerticalLine() {
+        image.drawVerticalLine(2, 3, 4, 'W');
+
+        Assert.that(image.getData(2,3) == 'W', "Error - image validation failed");
+        Assert.that(image.getData(2,4) == 'W', "Error - image validation failed");
+    }
+
+    @org.junit.Test
+    public void drawHorizontalLine() {
+        image.drawHorizontalLine(1, 5, 5, 'S');
+
+        Assert.that(image.getData(1,5) == 'S', "Error - image validation failed");
+        Assert.that(image.getData(2,5) == 'S', "Error - image validation failed");
+        Assert.that(image.getData(3,5) == 'S', "Error - image validation failed");
+        Assert.that(image.getData(4,5) == 'S', "Error - image validation failed");
+        Assert.that(image.getData(5,5) == 'S', "Error - image validation failed");
+    }
+
+    @org.junit.Test
+    public void fillRegion() {
+        image.drawHorizontalLine(1, 5, 2, 'H');
+        image.fillRegion(1,1, 'Q', 'x');
+
+        Assert.that(image.getData(1,1) == 'Q', "Error - image validation failed");
+        Assert.that(image.getData(2,1) == 'Q', "Error - image validation failed");
+        Assert.that(image.getData(3,1) == 'Q', "Error - image validation failed");
+        Assert.that(image.getData(4,1) == 'Q', "Error - image validation failed");
+        Assert.that(image.getData(5,1) == 'Q', "Error - image validation failed");
+
+        Assert.that(image.getData(1,2) == 'H', "Error - image validation failed");
+        Assert.that(image.getData(2,2) == 'H', "Error - image validation failed");
+        Assert.that(image.getData(3,2) == 'H', "Error - image validation failed");
+        Assert.that(image.getData(4,2) == 'H', "Error - image validation failed");
+        Assert.that(image.getData(5,2) == 'H', "Error - image validation failed");
+    }
 }
